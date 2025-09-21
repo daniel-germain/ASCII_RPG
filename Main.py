f00399fe50cddae53ac1602310117efaa80e52f3
daniel-germain <dricardogermain@gmail.com>
2025-09-20 21:22:56 -0400

    Added Battles, Fixed Loading Glitch

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-import os
+import os, random
 import Drawings
 
 # Booleans-------
@@ -7,22 +7,25 @@ menu = True
 play = False
 rules = False
 key = False
+fight = False
+standing = True
 # --------------
 
 
 # Stats -----------
 HP = 50
-HPMAX = 50
 ATK = 5
 health_pot = 1
 mana_pot = 1
 gold = 0
 x = 0
 y = 0
-stat_num = 10# Iterate if you add more stats. Used to error check
+HPMAX = 50
+stat_num = 10
+# Iterate if you add more stats. Used to error check
 # ---------------
 
-# Map
+# Map ---------------------------------
 game_map = [['hill', 'river', 'plains', 'cave', 'town', 'vally'],
        ['dungeon', 'vally', 'hill', 'river', 'town', 'cave'],
        ['cave', 'plains', 'hill', 'town', 'river', 'dungeon'],
@@ -77,6 +80,56 @@ print(enemy_tile)
 
 y_len = len(game_map) - 1
 x_len = len(game_map[0]) - 1
+# Map Info --------------------
+
+# OPPS!!!
+
+opp_list = ["Slime", "Goblin", "Skeleton"]
+
+opps = {
+    "Slime":
+        {
+            "HP" : 10,
+            "ATK" : 3,
+            "GOLD": 3
+        },
+    "Goblin":
+        {
+            "HP": 8,
+            "ATK": 5,
+            "GOLD": 4
+        },
+    "DEMON LORD DAN":
+        {
+            "HP": 100,
+            "ATK": 10,
+            "GOLD": 1000
+        },
+
+    "Skeleton":
+        {
+            "HP": 10,
+            "ATK": 5,
+            "GOLD": 8
+        },
+
+    "ORC":
+        {
+            "HP": 16,
+            "ATK": 8,
+            "GOLD": 30
+        },
+
+    "DRAGON":
+        {
+            "HP": 60,
+            "ATK": 8,
+            "GOLD": 100
+        }
+
+}
+
+
 # Clear --------
 def clear():
     os.system("clear")
@@ -104,6 +157,96 @@ def save():
         f.write(item + "\n")
     f.close()
 # -----------------------------
+
+# Gameplay Functions
+def heal(amount):
+    global HP
+    if HP + amount < HPMAX:
+        HP += amount
+    else:
+        HP = HPMAX
+
+    print(f"{name} has gained {amount} HP!")
+
+def battle():
+
+    global fight, play, run, HP, health_pot, mana_pot, gold
+
+    enemy = random.choice(opp_list)
+    enemy_hp = opps[enemy]["HP"]
+    enemy_MAXHP = enemy_hp
+    enemy_atk = opps[enemy]["ATK"]
+    enemy_gold = opps[enemy]["GOLD"]
+
+    while fight:
+        clear()
+        Drawings.draw_line()
+        print(f"Defeat the {enemy}!")
+        Drawings.draw_line()
+        print(f"{enemy}'s HP: {enemy_hp}/{enemy_MAXHP}")
+        print(f"{name}'s HP: {HP}/{HPMAX}")
+        print(f"{name}'s Health Potions: {health_pot}")
+        print(f"{name}'s Mana Potions: {mana_pot}")
+        Drawings.draw_line()
+        print("1 - Attack")
+        if health_pot > 0:
+            print("2 - Use Health Potion")
+        elif mana_pot > 0:
+            print("3 - Use Mana Potion")
+        Drawings.draw_line()
+
+        choice = int(input("# "))
+
+        if choice == 1:
+            enemy_hp -= ATK
+            print(f"{name} did {ATK} damage to {enemy}.")
+            if enemy_hp > 0:
+                HP -= enemy_atk
+                print(f"{enemy} did {enemy_atk} damage to {name}")
+                input("> ")
+
+        elif choice == 2:
+            if health_pot > 0:
+                health_pot -= 1
+                heal(30)
+                HP - enemy_atk
+                print(f"{enemy} did {enemy_atk} damage to {name}")
+                input("> ")
+            else:
+                print("You have no potions!")
+
+        elif choice == 3:
+            if mana_pot > 0:
+                mana_pot -= 1
+                heal(50)
+                HP - enemy_atk
+                print(f"{enemy} did {enemy_atk} damage to {name}")
+                input("> ")
+            else:
+                print("You have no potions!")
+
+        if HP <= 0:
+            print(f"{enemy} defeated {name}")
+            fight = False
+            run = False
+            play = False
+            print("GAME OVER")
+            input("> ")
+            quit()
+
+        if enemy_hp < 0:
+            print(f"{name} defeated {enemy}")
+            fight = False
+
+            # Drop Rewards!!
+            gold += enemy_gold
+            if random.randint(0, 100) < 30:
+                health_pot += 1
+                print("You found a health potion!")
+
+            if random.randint(0, 100) < 10:
+                mana_pot += 1
+                print("You found a mana potion!")
 
 
 # Gameplay Loop ----------
@@ -146,13 +289,14 @@ while run:
                 if len(load_list) == stat_num:
                     name = load_list[0][:-1]
                     HP = int(load_list[1][:-1])
-                    ATK = int(load_list[2][:-1])
-                    health_pot = int(load_list[3][:-1])
-                    mana_pot = int(load_list[4][:-1])
-                    gold = int(load_list[5][:-1])
-                    x = int(load_list[6][:-1])
-                    y = int(load_list[7][:-1])
-                    key = bool(load_list[8][:-1])
+                    HPMAX = int(load_list[2][:-1])
+                    ATK = int(load_list[3][:-1])
+                    health_pot = int(load_list[4][:-1])
+                    mana_pot = int(load_list[5][:-1])
+                    gold = int(load_list[6][:-1])
+                    x = int(load_list[7][:-1])
+                    y = int(load_list[8][:-1])
+                    key = bool(load_list[9][:-1])
                     print(f"Welcome back \"{name}\"")
                     input("> ")
                     menu = False
@@ -171,8 +315,19 @@ while run:
 
     while play:
         save() # Auto Save
-
         clear()
+
+        # Combat Encounter!!! ----------
+        if not standing:
+            if biom[game_map[y][x]]["e"]:
+                if random.randint(0, 100) <= 30:
+                    fight = True
+                    clear()
+                    battle()
+
+        # ---------------------
+
+
         Drawings.draw_line()
         print(f"Location: " + biom[game_map[y][x]]["t"])
         Drawings.draw_line()
@@ -189,10 +344,12 @@ while run:
         print("2 - EAST")
         print("3 - SOUTH")
         print("4 - WEST")
+        print("5 - Drink Health Potion")
+        print("6 - Drink Mana Potion")
         Drawings.draw_line()
         dest = input("# ")
 
-        # Loop to Send you Back to the Menu
+        # Gameplay Options!
         if dest == "0":
             play = False
             menu = True
@@ -200,21 +357,51 @@ while run:
         elif dest == "1":
             if y > 0:
                 y -= 1
+                standing = False
             else:
                 y = y_len
+                standing = False
         elif dest == "2":
             if x < x_len:
                 x += 1
+                standing = False
             else:
                 x = 0
+                standing = False
         elif dest == "3":
             if y < y_len:
                 y += 1
+                standing = False
             else:
                 y = 0
+                standing = False
         elif dest == "4":
             if x > 0:
                 x -= 1
+                standing = False
             else:
                 x = x_len
+                standing = False
+        elif dest == "5":
+            if health_pot > 0:
+                health_pot -= 1
+                heal(30)
+                print(f"{name} gained 30 HP!")
+                input("> ")
+            else:
+                print("You have no potions!")
+            input("> ")
+            standing = True
+        elif dest == "6":
+            if mana_pot > 0:
+                mana_pot -= 1
+                heal(50)
+                print(f"{name} gained 30 HP!")
+                input("> ")
+            else:
+                print("You have no potions!")
+
+            input("> ")
+            standing = True
+
 # -------------------
